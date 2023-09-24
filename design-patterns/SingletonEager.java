d1ab905f4e19b5ab56f5f421748deac05544fd22
erikvsuris <erikvsuris@gmail.com>
2023-09-24 19:46:23 -0300

    Maven e Design Patterns

---
+++ design-patterns/SingletonEager.java
@@ -0,0 +1,17 @@
+/**
+ * Singleton "Apressado"
+ * 
+ * @author erikvsuris
+ */
+
+public class SingletonEager {
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager() {
+        super();
+    }
+
+    public static SingletonEager getInstancia() {
+        return instancia;
+    }
+}
---
+++ design-patterns/SingletonLazy.java
@@ -0,0 +1,18 @@
+/**
+ * Singleton "Preguiçoso"
+ * 
+ * @author erikvsuris
+ */
+
+public class SingletonLazy {
+    private static SingletonLazy instancia;
+
+    private SingletonLazy() {
+        super();
+    }
+
+    public static SingletonLazy getInstancia() {
+        if (instancia == null) instancia = new SingletonLazy();
+        return instancia;
+    }
+}
